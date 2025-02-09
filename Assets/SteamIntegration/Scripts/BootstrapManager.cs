209cbf0dda417d006021cbd884c5a3dd2977b9df
committ <89640845+spaceComplexitys@users.noreply.github.com>
2025-02-09 15:49:12 -0500

    Everything works until 6:36 of tutorial

--- Assets/SteamIntegration/Scripts/BootstrapManager.cs
+++ Assets/SteamIntegration/Scripts/BootstrapManager.cs
@@ -1,9 +1,14 @@
+using FishNet.Managing;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class BootstrapManager : MonoBehaviour
 {
     [SerializeField] private string menuName = "MenuSceneSteam";
+    [SerializeField] private NetworkManager _networkManager;
+    [SerializeField] private FishySteamworks.FishySteamworks _fishySteamworks;
+    
+
 
     public void GoToMenu() {
         SceneManager.LoadScene(menuName, LoadSceneMode.Additive);
